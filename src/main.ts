39b7be4f86dbebc771007e25d1565cbbc6f5d1f1
tomhine792 <tomhine792@outlook.com>
2022-06-25 14:32:52 +0100

    fix title, add basic box code

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,29 @@ import "./style.css";
 import * as t from "three";
 
 const canvas = document.querySelector<HTMLCanvasElement>("[data-webgl]")!;
+
+const scene = new t.Scene();
+
+// Object
+const geometry = new t.BoxGeometry(1, 1, 1);
+const material = new t.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new t.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Camera
+const camera = new t.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new t.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
